5ea02dab66f0dfc8360e9c0a96620ad6df89c244
Alejandro <aasalazar23@gmail.com>
2019-10-10 15:20:54 -0600

    Use setState callback for query search
    SearchTerm and query were falling out of sync because the setState
    cycle hadn't completed. This was fixed by moving the fetch logic to a
    callback in setState. This ensured that the query was set before being
    passed to the BookAPI.

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,58 +8,48 @@ export class Search extends React.Component {
     super(props);
     this.state = {
       query: "",
-      results: [],
       books: [],
     };
   }
-  checkedBooks = (results) => {
+  checkedBooks = results => {
     const shelvedBooks = this.props.shelvedBooks;
     const ids = shelvedBooks.map(book => book.id);
 
-    if (results.error) {
-      this.setState({ books: [] })
+    if (!results || results.error) {
+      this.setState({ books: [] });
     } else {
-      const books = results.map((result) => {
+      const books = results.map(result => {
         if (ids.includes(result.id)) {
-          return shelvedBooks.find(book => book.id === result.id)
+          return shelvedBooks.find(book => book.id === result.id);
         }
         return result;
-      })
+      });
       this.setState({ books });
     }
-
-  }
+  };
 
   handleQuery = event => {
-    const { query } = this.state;
-    console.log(query);
     const searchTerm = event.target.value;
-    this.setState({ query: searchTerm });
-    if (searchTerm) {
-      BooksAPI.search(searchTerm).then(results => {
-        this.checkedBooks(results);
-        this.setState({ results: results });
-      });
-    } else {
-        this.setState({ results: [] });
-    }
-
+    this.setState({ query: searchTerm }, () => {
+      const { query } = this.state;
+      if (query) {
+        BooksAPI.search(query).then(results => {
+          this.checkedBooks(results);
+        });
+      } else {
+        this.setState({ books: [] });
+      }
+    });
   };
 
   render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <Link to='/' className="close-search">Close</Link>
+          <Link to="/" className="close-search">
+            Close
+          </Link>
           <div className="search-books-input-wrapper">
-            {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
             <input
               type="text"
               placeholder="Search by title or author"
@@ -70,8 +60,12 @@ export class Search extends React.Component {
         </div>
 
         <div className="search-books-results">
-                {/* TODO: if shelved book in results, pass shelf to book render */}
-          <BookShelf books={this.state.books} onShelfChange={this.props.onShelfChange}>Results</BookShelf>
+          <BookShelf
+            books={this.state.books}
+            onShelfChange={this.props.onShelfChange}
+          >
+            Results
+          </BookShelf>
         </div>
       </div>
     );
